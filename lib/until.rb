52addee19230a00d8c5b0f8b13c4b47f5216dc96
Tra To <tothutra@gmail.com>
2018-08-12 03:14:24 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 def using_until
     evitation_force = 6
         #your code here
-    until evitation_force>10
+    until evitation_force==10
       evitation_force +=1
-      print 'Wingardium Leviosa\n'
+      puts 'Wingardium Leviosa'
     end
 end
